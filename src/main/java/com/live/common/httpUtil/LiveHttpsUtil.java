03e5ce14b1ccfc270dd8deea8271f6f892ce6571
live <sunxn1991@163.com>
2022-03-14 17:21:35 +0800

    https工具，补充get方式

--- src/main/java/com/live/common/httpUtil/LiveHttpsUtil.java
+++ src/main/java/com/live/common/httpUtil/LiveHttpsUtil.java
@@ -2,6 +2,7 @@ package com.live.common.httpUtil;
 
 
 import com.live.common.LiveConstant;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.net.ssl.*;
 import java.io.BufferedReader;
@@ -26,14 +27,30 @@ import java.util.Map;
 public class LiveHttpsUtil {
 
     public static final String DEBUG_TIP = "\t________debugTip(" + LiveHttpsUtil.class.getSimpleName() + ")";
-    private static LiveSSLUtil sslUtil;
+
+    /**
+     * 注意客户端发送请求（Request）时的Content-Type设置
+     * <p>
+     * 建议：
+     * <p>
+     * 如果是一个restful接口（json格式），一般将Content-Type设置为application/json; charset=UTF-8；
+     * 如果是文件上传，一般Content-Type设置为multipart/form-data
+     * 如果普通表单提交，一般Content-Type设置为application/x-www-form-urlencoded
+     */
+    public static final String HTTP_CONTENT_TYPE_JSON = "application/json";
+    public static final String HTTP_CONTENT_TYPE_URLENCODED = "application/x-www-form-urlencoded";
+
+    public static final String HTTP_REQUEST_METHOD_POST = "POST";
+    public static final String HTTP_REQUEST_METHOD_GET = "GET";
+
+//    private static LiveSSLUtil sslUtil;
 
     /**
      * @param httpUrl
      * @return
      */
-    public static String post(String httpUrl) {
-        return post(httpUrl);
+    public static String post(String httpUrl) throws Exception {
+        return post(httpUrl, "");
     }
 
     public static String post(String httpUrl, Map<String, String> params) throws Exception {
@@ -41,8 +58,9 @@ public class LiveHttpsUtil {
         Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<String, String> entry = iterator.next();
-            if (!param.equals(""))
+            if (!param.equals("")) {
                 param = param.concat("&");
+            }
             param = param.concat(entry.getKey()).concat("=")
                     .concat(entry != null ? entry.getValue() : "");
         }
@@ -60,8 +78,26 @@ public class LiveHttpsUtil {
     }
 
     public static String post(String httpUrl, String param, LinkedHashMap<String, String> httpHeaders) throws Exception {
+        return post(httpUrl, param, httpHeaders, null);
+    }
+
+    public static String get(String httpUrl, String param, LinkedHashMap<String, String> httpHeaders, String httpContenType) throws Exception {
+        return request(httpUrl, param, httpHeaders, httpContenType, HTTP_REQUEST_METHOD_GET);
+    }
+
+    public static String get(String httpUrl) throws Exception {
+        return get(httpUrl, "", null, HTTP_REQUEST_METHOD_GET);
+    }
+
+    public static String post(String httpUrl, String param, LinkedHashMap<String, String> httpHeaders, String httpContenType) throws Exception {
+        return request(httpUrl, param, httpHeaders, httpContenType, HTTP_REQUEST_METHOD_POST);
+    }
+
+    public static String request(String httpUrl, String param, LinkedHashMap<String, String> httpHeaders, String httpContenType, String requestMethod) throws Exception {
         StringBuilder result = new StringBuilder();
-        LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求地址", param);
+        LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求方法（method）", requestMethod);
+        LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求地址", httpUrl);
+        LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求参数", param);
         LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求参数（httpHeaders）", httpHeaders);
         LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求参数（boday）", param);
 
@@ -76,7 +112,7 @@ public class LiveHttpsUtil {
         LiveConstant.LogConstant.beautifyByTip(DEBUG_TIP, "请求协议", url.getProtocol());
         if ("https".equalsIgnoreCase(url.getProtocol())) {
             try {
-                sslUtil.ignoreSsl();
+                LiveSSLUtil.ignoreSsl();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -88,9 +124,15 @@ public class LiveHttpsUtil {
             urlCon = (HttpURLConnection) url.openConnection();
             urlCon.setDoInput(true);
             urlCon.setDoOutput(true);
-            urlCon.setRequestMethod("POST");
-            urlCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//            urlCon.setRequestProperty("Content-type", "application/json");
+            //GET or POST
+            if (httpContenType != null && StringUtils.isNotBlank(requestMethod)) {
+                urlCon.setRequestMethod(requestMethod);
+            }
+
+//            urlCon.setRequestProperty("Content-type", HTTP_CONTENT_TYPE_JSON);
+            if (httpContenType != null && StringUtils.isNotBlank(httpContenType)) {
+                urlCon.setRequestProperty("Content-Type", httpContenType);
+            }
 
             if (httpHeaders != null && httpHeaders.size() > 0) {
                 HttpURLConnection finalUrlCon = urlCon;
@@ -100,10 +142,12 @@ public class LiveHttpsUtil {
             }
 
             // 设置为utf8可以解决服务器接收时读取的数据中文乱码问题
-            OutputStreamWriter out = new OutputStreamWriter(urlCon.getOutputStream(), LiveConstant.CharsetConstant.UTF8);
-            out.write(param);
-            out.flush();
-            out.close();
+            if (param != null && StringUtils.isNotBlank(param)) {
+                OutputStreamWriter out = new OutputStreamWriter(urlCon.getOutputStream(), LiveConstant.CharsetConstant.UTF8);
+                out.write(param);
+                out.flush();
+                out.close();
+            }
 
             in = new BufferedReader(new InputStreamReader(
                     urlCon.getInputStream(), LiveConstant.CharsetConstant.UTF8));
@@ -159,6 +203,7 @@ public class LiveHttpsUtil {
          */
         public static void ignoreSsl() throws Exception {
             HostnameVerifier hv = new HostnameVerifier() {
+                @Override
                 public boolean verify(String urlHostName, SSLSession session) {
                     LiveConstant.LogConstant.beautify("host", urlHostName, "port", session.getPeerPort());
                     return true;
@@ -173,6 +218,7 @@ public class LiveHttpsUtil {
          */
         private static class LiveX509TM implements TrustManager, X509TrustManager {
 
+            @Override
             public X509Certificate[] getAcceptedIssuers() {
                 return null;
             }
@@ -186,6 +232,7 @@ public class LiveHttpsUtil {
             }
 
             //信任服务器的证书
+            @Override
             public void checkServerTrusted(X509Certificate[] certs, String authType)
                     throws CertificateException {
                 try {
@@ -197,10 +244,20 @@ public class LiveHttpsUtil {
                 return;
             }
 
+            @Override
             public void checkClientTrusted(X509Certificate[] certs, String authType)
                     throws CertificateException {
                 return;
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+//            LiveHttpsUtil.post("https://www.baidu.com");
+//            LiveHttpsUtil.get("https://www.baidu.com/s?ie=UTF-8&wd=contenttype");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
